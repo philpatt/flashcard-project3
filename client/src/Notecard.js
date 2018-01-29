b6087f28a562acac972e4f341e1223122d293e9f
peterpickles <lipeter2006@gmail.com>
2018-01-29 15:23:26 -0800

    added component stubs, all page files, removed logo, made decks on decks

---
+++ client/src/Notecard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Details from './Details';
+
+
+
+class Notecard extends Component {
+    render () {
+        return (
+        
+            <div><h4>A single notecard for quiz mode mode!</h4>
+            <Details />
+            </div>)
+        
+    }
+}
+
+export default Notecard;
