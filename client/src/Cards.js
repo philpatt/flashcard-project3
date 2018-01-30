961ce1741bfe89c59457100ffd6a1bb8fa590596
philpatt <phillip.s.patterson1@gmail.com>
2018-01-29 21:55:31 -0800

    working on deck page and individual cards to show, added react-bootstrap cdn

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
-
+import './CardStyle.css';
+import { Button } from 'react-bootstrap';
 
 class Cards extends Component {
+let categoryCards;
+categoryCards = this.props.oneCard
     render () {
-        return (<div>Cards Cards Cards</div>)
+        console.log('this is my cards object',this.props.oneCard)
+        return (
+            <ul className='all-cards'>
+                <li className='card-box'>
+                    <p><strong>Category</strong> : <i>{this.props.oneCard.category}</i></p>
+                    <p><strong>Question</strong>: <i>{this.props.oneCard.question}</i></p>
+                    <p><strong>Answer</strong>: <i>{this.props.oneCard.answer}</i></p>
+                    <Button bsStyle='danger' >Delete</Button>                    
+                </li>
+            </ul>
+        )
     }
 }
 
--- client/src/Deck.js
+++ client/src/Deck.js
@@ -1,15 +1,69 @@
 import React, {Component} from 'react';
 import Cards from './Cards.js';
 
-
-
 class Deck extends Component {
-    render () {
+    constructor(props){
+        super(props);
+        // setting state to fake cards
+        this.state = {
+            cards: [
+                {
+                    question: 'Is math important?',
+                    answer: 'Only if you want to know it.',
+                    category: 'Math'
+                },
+                {
+                    question: 'How do you become rich and famous?',
+                    answer: 'Become a coder',
+                    category: 'Code'
+                },
+                {
+                    question: 'Is beer good?',
+                    answer: 'Obviously',
+                    category: 'Beer'
+                }, 
+                {
+                    question: 'Is math important?',
+                    answer: 'Only if you want to know it.',
+                    category: 'Math'
+                },
+                {
+                    question: 'How do you become rich and famous?',
+                    answer: 'Become a coder',
+                    category: 'Code'
+                },
+                {
+                    question: 'What is beer?',
+                    answer: 'Delicious.',
+                    category: 'Beer'
+                }
+            ]
+        }
+    }
+
+     render () {
+        //  filter state to only show by one category
+        // maybe the button the sidebar will do this function?
+        //  iterate through state to break up object array to show individual cards
+        let deck;
+        if(this.state.cards){
+            deck = this.state.cards
+            .filter(function (item) {
+                return item.category == 'Beer';
+            })
+            .map(card => {
+                console.log('DUUUUUDEEEEE THIS IS A CARD',card);
+                return(
+                    <Cards oneCard={card} />
+                )
+            })
+        }
+        console.log('########',this.state.cards);
         return (
-        
-            <div>Deck Deck Deck
-                <Cards />
-            </div>)
+            <div className='deck-container'>
+                { deck }
+            </div>
+        )
         
     }
 }
