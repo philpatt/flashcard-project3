f0b7a2c8cce7ab0c6139cb51e2ac505bb128ee4b
philpatt <phillip.s.patterson1@gmail.com>
2018-02-01 14:07:11 -0800

    git merge issues

--- client/src/auth/Logout.js
+++ client/src/auth/Logout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
 class Logout extends Component {
+  
   constructor(props){
     super(props);
     this.state = {
